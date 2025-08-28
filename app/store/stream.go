fbb121a9c4666f6e470f69047a8bca8c7650e16a
yanshuy <yanshuman2005@gmail.com>
2025-08-28 18:17:08 +0530

    codecrafters submit [skip ci]

--- app/store/stream.go
+++ app/store/stream.go
@@ -15,16 +15,33 @@ func (rs *RedisStore) Xadd(key, stream_key string, key_vals []string) error {
 	if err != nil {
 		return errors.New("invalid stream key")
 	}
-	sqNo, err := strconv.Atoi(parts[1])
+	var sqNo int
+	m, exists := rs.Look(key)
+	if exists && parts[0] == "*" {
+		for _, item := range m.data.Stream {
+			if item.time_ms == time_ms {
+				sqNo = item.sequenceNo + 1
+			}
+		}
+	}
+	sqNo, err = strconv.Atoi(parts[1])
 	if err != nil {
 		return errors.New("invalid stream key")
 	}
+	if time_ms == 0 {
+		sqNo = 1
+	}
+
 	if time_ms < 0 || sqNo < 0 {
 		return errors.New("The ID specified in XADD must be greater than 0-0")
 	}
 
-	var entry StreamEntry
-	if m, ok := rs.Look(key); ok {
+	entry := StreamEntry{
+		sequenceNo: sqNo,
+		time_ms:    time_ms,
+	}
+
+	if exists {
 		last := m.data.Stream[len(m.data.Stream)-1]
 		if time_ms < last.time_ms {
 			if time_ms == 0 && sqNo == 0 {
@@ -35,26 +52,19 @@ func (rs *RedisStore) Xadd(key, stream_key string, key_vals []string) error {
 		if time_ms == last.time_ms && sqNo <= last.sequenceNo {
 			return errors.New("The ID specified in XADD is equal or smaller than the target stream top item")
 		}
-		entry = StreamEntry{
-			sequenceNo: sqNo,
-			time_ms:    time_ms,
-			fields:     make(map[string]string),
-		}
 		m.data.Stream = append(m.data.Stream, entry)
-
 	} else {
-		entry = StreamEntry{
-			sequenceNo: sqNo,
-			time_ms:    time_ms,
-			fields:     make(map[string]string),
-		}
 		mNew := rs.NewStoreMember(key, Stream)
 		mNew.data.Stream = append(mNew.data.Stream, entry)
 	}
-	for i := 0; i < len(key_vals); i += 2 {
-		key := key_vals[i]
-		val := key_vals[i+1]
-		entry.fields[key] = val
+
+	if len(key_vals) > 1 {
+		entry.fields = make(map[string]string)
+		for i := 0; i < len(key_vals); i += 2 {
+			key := key_vals[i]
+			val := key_vals[i+1]
+			entry.fields[key] = val
+		}
 	}
 	return nil
 }
