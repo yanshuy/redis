0599701b18271a3ca83630b26326df3a9ea0dbad
Yanshuman Yadav <yanshuman2005@gmail.com>
2026-08-01 18:12:04 +0530

    codecrafters submit [skip ci]

--- app/request/acl.go
+++ app/request/acl.go
@@ -13,7 +13,7 @@ func ACL_GETUSER(user resp.DataType) resp.DataType {
 		return resp.NewData(resp.BulkString, "-1")
 	}
 	flags := resp.NewData(resp.BulkString, "flags")
-	flags_v := resp.NewData(resp.Array, []string{})
+	flags_v := resp.NewData(resp.Array, []string{"nopass"})
 
 	res := []resp.DataType{flags, flags_v}
 	return resp.NewData(resp.Array, res)
