d862bcf32a3c51077a345029c4b6b7510e254d35
Yanshuman Yadav <yanshuman2005@gmail.com>
2026-08-01 18:31:38 +0530

    codecrafters submit [skip ci]

--- app/request/acl.go
+++ app/request/acl.go
@@ -4,6 +4,31 @@ import (
 	resp "github.com/codecrafters-io/redis-starter-go/app/RESP"
 )
 
+type User struct {
+	name      string
+	flags     []string
+	passwords []string
+}
+
+func (u *User) ToRESP() resp.DataType {
+	flags := resp.NewData(resp.BulkString, "flags")
+	flags_v := resp.NewData(resp.Array, u.flags)
+
+	passwords := resp.NewData(resp.BulkString, "passwords")
+	passwords_v := resp.NewData(resp.Array, u.passwords)
+
+	res := []resp.DataType{flags, flags_v, passwords, passwords_v}
+	return resp.NewData(resp.Array, res)
+}
+
+var users = map[string]User{
+	"default": {
+		name:      "default",
+		flags:     []string{"nopass"},
+		passwords: []string{},
+	},
+}
+
 func ACL_WHOAMI() resp.DataType {
 	return resp.NewData(resp.BulkString, "default")
 }
@@ -12,9 +37,11 @@ func ACL_GETUSER(user resp.DataType) resp.DataType {
 	if user.Type != resp.BulkString {
 		return resp.NewData(resp.BulkString, "-1")
 	}
-	flags := resp.NewData(resp.BulkString, "flags")
-	flags_v := resp.NewData(resp.Array, []string{"nopass"})
+	username := user.Str
 
-	res := []resp.DataType{flags, flags_v}
-	return resp.NewData(resp.Array, res)
+	if user, ok := users[username]; ok {
+		return user.ToRESP()
+	} else {
+		panic("user does not exists")
+	}
 }
