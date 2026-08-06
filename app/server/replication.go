b5b19cc9576c24569d3e1b590c0b89542197da46
Yanshuman Yadav <yanshuman2005@gmail.com>
2026-08-07 01:48:13 +0530

    codecrafters submit [skip ci]

--- app/server/replication.go
+++ app/server/replication.go
@@ -39,6 +39,20 @@ func (s *Server) HandshakeMaster() error {
 		return err
 	}
 
+	psync := resp.NewData(resp.Array, []string{"PSYNC", "?", "-1"})
+	_, err = conn.Write(psync.ToResponse())
+	if err != nil {
+		return fmt.Errorf("failed to send message to peer")
+	}
+	resync, err := r.ReadRESP(conn)
+	if err != nil {
+		return fmt.Errorf("failed to read message from peer")
+	}
+	fields := strings.Fields(resync.Str)
+	repl_id := fields[1]
+	repl_off := fields[2]
+	fmt.Print(repl_id, repl_off)
+
 	return nil
 }
 
